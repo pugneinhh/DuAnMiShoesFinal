5a7baff8940344babc62bd4f229f8220fa13e83c
QUANGANH\kazee <anhpqph29035@fpt.edu.vn>
2024-01-14 11:30:39 +0700

    Test Thong Ke

--- reactapp/src/components/thongKe/ThongKe.js
+++ reactapp/src/components/thongKe/ThongKe.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { Pie } from '@ant-design/plots';
 import { Divider } from 'antd';
 import { RxDashboard } from 'react-icons/rx';
+import { Line } from '@ant-design/plots';
+
 import axios from 'axios';
 
 export default function ThongKe() {
@@ -101,7 +103,30 @@ export default function ThongKe() {
         ],
     };
 
+    // Biểu đồ đường
+    const [dataDuong, setDataDuong] = useState([]);
 
+    useEffect(() => {
+      asyncFetch();
+    }, []);
+  
+    const asyncFetch = () => {
+      fetch('https://gw.alipayobjects.com/os/bmw-prod/1d565782-dde4-4bb6-8946-ea6a38ccf184.json')
+        .then((response) => response.json())
+        .then((json) => setData(json))
+        .catch((error) => {
+          console.log('fetch data failed', error);
+        });
+    };
+    const configDuong = {
+      data,
+      padding: 'auto',
+      xField: 'Date',
+      yField: 'scales',
+      xAxis: {
+        tickCount: 5,
+      },
+    };
 
     return(
         <div className='container-fluid'>
@@ -162,6 +187,10 @@ export default function ThongKe() {
             {/* biểu đồ tròn */}
             <div className='col-md-4'>
                 <Pie {...config} />
+
+            </div>
+            <div className='col-md-4'>
+                <Line {...configDuong} />
 
             </div>
         </div>
