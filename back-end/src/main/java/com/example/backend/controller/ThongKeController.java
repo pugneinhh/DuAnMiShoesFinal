a7dc9fe05d293a84fda292cf861bf3ed7195ffab
anhntpph28990@fpt.edu.vn <anhntpph28990@fpt.edu.vn>
2024-01-21 19:52:07 +0700

    thống kê

--- back-end/src/main/java/com/example/backend/controller/ThongKeController.java
+++ back-end/src/main/java/com/example/backend/controller/ThongKeController.java
@@ -33,31 +33,66 @@ public class ThongKeController {
     public ResponseEntity<?> thongKeTheoNam(){
         return ResponseEntity.ok(thongKeService.thongKeTheoNam());
     }
+    @GetMapping("/doanh-thu-ngay-truoc")
+    public ResponseEntity<?> doanhThuNgayTruoc(){
+        return ResponseEntity.ok(thongKeService.getDoanhThuNgayTruoc());
+    }
+    @GetMapping("/doanh-thu-thang-truoc")
+    public ResponseEntity<?> doanhThuThangTruoc(){
+        return ResponseEntity.ok(thongKeService.getDoanhThuThangTruoc());
+    }
+    @GetMapping("/doanh-thu-nam-truoc")
+    public ResponseEntity<?> doanhThuNamTruoc(){
+        return ResponseEntity.ok(thongKeService.getDoanhThuNamTruoc());
+    }
 
-    @GetMapping("/san-pham-ban-chay")
-    public ResponseEntity<?> getSPBanChay(){
-        return ResponseEntity.ok(thongKeService.getSpBanChay());
+    @GetMapping("/san-pham-ban-chay-ngay")
+    public ResponseEntity<?> getSPBanChayNgay(){
+        return ResponseEntity.ok(thongKeService.getSpBanChayNgay());
     }
-    @GetMapping("/bieu-do-ngay/{ngay}")
-    public ResponseEntity<?> getBieuDoNgay(@PathVariable("ngay")String ngay) throws ParseException {
-        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
-        java.util.Date d=sdf.parse(ngay);
-        Date date=new Date(d.getTime());
-        return ResponseEntity.ok(thongKeService.getBieuDoNgay(date));
+    @GetMapping("/san-pham-ban-chay-thang")
+    public ResponseEntity<?> getSPBanChayThang(){
+        return ResponseEntity.ok(thongKeService.getSpBanChayThang());
     }
-    @GetMapping("/bieu-do-tuan/{ngay}")
-    public ResponseEntity<?> getBieuDoTuan(@PathVariable("ngay")String ngay) throws ParseException {
-        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
-        java.util.Date d=sdf.parse(ngay);
-        Date date=new Date(d.getTime());
-        return ResponseEntity.ok(thongKeService.getBieuDoTuan(date));
+    @GetMapping("/san-pham-ban-chay-nam")
+    public ResponseEntity<?> getSPBanChayNam(){
+        return ResponseEntity.ok(thongKeService.getSpBanChayNam());
+    }
+    @GetMapping("/san-pham-ban-chay-tuan")
+    public ResponseEntity<?> getSPBanChayTuan(){
+        return ResponseEntity.ok(thongKeService.getSpBanChayTuan());
+    }
+    @GetMapping("/bieu-do-ngay")
+    public ResponseEntity<?> getBieuDoNgay() throws ParseException {
+        return ResponseEntity.ok(thongKeService.getBieuDoNgay());
+    }
+    @GetMapping("/bieu-do-tuan")
+    public ResponseEntity<?> getBieuDoTuan() throws ParseException {
+
+        return ResponseEntity.ok(thongKeService.getBieuDoTuan());
     }
     @GetMapping("/bieu-do-thang")
     public ResponseEntity<?> getBieuDoThang() throws ParseException {
         return ResponseEntity.ok(thongKeService.getBieuDoThang());
     }
-    @GetMapping("/trang-thai-hoa-don")
-    public ResponseEntity<?> getTrangThaiHD(){
-        return ResponseEntity.ok(thongKeService.getTrangThaiHoaDon());
+    @GetMapping("/bieu-do-nam")
+    public ResponseEntity<?> getBieuDoNam() throws ParseException {
+        return ResponseEntity.ok(thongKeService.getBieuDoNam());
+    }
+    @GetMapping("/trang-thai-hoa-don-ngay")
+    public ResponseEntity<?> getTrangThaiHDNgay(){
+        return ResponseEntity.ok(thongKeService.getTrangThaiHoaDonNgay());
+    }
+    @GetMapping("/trang-thai-hoa-don-thang")
+    public ResponseEntity<?> getTrangThaiHDThang(){
+        return ResponseEntity.ok(thongKeService.getTrangThaiHoaDonThang());
+    }
+    @GetMapping("/trang-thai-hoa-don-nam")
+    public ResponseEntity<?> getTrangThaiHDNam(){
+        return ResponseEntity.ok(thongKeService.getTrangThaiHoaDonNam());
+    }
+    @GetMapping("/trang-thai-hoa-don-tuan")
+    public ResponseEntity<?> getTrangThaiHDTuan(){
+        return ResponseEntity.ok(thongKeService.getTrangThaiHoaDonTuan());
     }
 }
