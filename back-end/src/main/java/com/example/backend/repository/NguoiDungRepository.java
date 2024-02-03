862880e890c7344e393e5f59cb2fe7483378fb01
Paiigio <duongntph29011@fpt.edu.vn>
2024-02-03 13:15:18 +0700

    check sdt , check email nhân viên khách hàng

--- back-end/src/main/java/com/example/backend/repository/NguoiDungRepository.java
+++ back-end/src/main/java/com/example/backend/repository/NguoiDungRepository.java
@@ -15,10 +15,10 @@ import java.util.UUID;
 
 public interface NguoiDungRepository extends JpaRepository<NguoiDung, String> {
     @Query(value = "SELECT CASE WHEN nd.diem IS NULL THEN N'0'ELSE nd.diem END  as diem, nd.id AS idND, nd.ma AS maND, nd.ten AS tenND, nd.so_dien_thoai AS SDT,\n" +
-            "  CASE WHEN nd.email IS NULL THEN N'Không có'ELSE nd.email END  as email,CASE WHEN nd.ngay_sinh IS NULL THEN N'Không có'ELSE nd.ngay_sinh END  as ngaySinh\n" +
-            "  ,nd.gioi_tinh AS gioiTinh, nd.chung_minh_thu AS cccd, CASE WHEN nd.anh IS NULL THEN N'Không có'ELSE nd.anh END  as anh, \n" +
-            "  nd.trang_thai AS trangThai\n" +
-            "FROM nguoi_dung nd WHERE nd.chuc_vu = 'nhan_vien' order by maND desc ", nativeQuery = true)
+            "             CASE WHEN nd.email IS NULL THEN N'Không có'ELSE nd.email END  as email,CASE WHEN nd.ngay_sinh IS NULL THEN N'Không có'ELSE nd.ngay_sinh END  as ngaySinh\n" +
+            "             ,nd.gioi_tinh AS gioiTinh, nd.chung_minh_thu AS cccd, CASE WHEN nd.anh IS NULL THEN N'Không có'ELSE nd.anh END  as anh,\n" +
+            "             nd.trang_thai AS trangThai \n" +
+            "            FROM nguoi_dung nd WHERE nd.chuc_vu = 'nhan_vien' order by ngay_tham_gia desc ", nativeQuery = true)
     List<AdminNhanVienRespon> getAllNhanVien();
 
     @Query(value = """
@@ -50,8 +50,7 @@ public interface NguoiDungRepository extends JpaRepository<NguoiDung, String> {
                  nguoi_dung nd
              where
                  nd.chuc_vu = 'khach_hang'
-             order by
-                 maND desc
+         order by ngay_tham_gia desc
              """, nativeQuery = true)
     List<AdminKhachHangRepon> getAllKhachHang();
 
