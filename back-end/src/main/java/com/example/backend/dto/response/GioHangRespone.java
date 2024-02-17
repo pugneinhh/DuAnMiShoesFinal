e4b694cef533718698aab8c9de8727ca98cf9fce
anhntpph28990@fpt.edu.vn <anhntpph28990@fpt.edu.vn>
2024-02-17 17:00:15 +0700

    gio hang

--- back-end/src/main/java/com/example/backend/dto/response/GioHangRespone.java
+++ back-end/src/main/java/com/example/backend/dto/response/GioHangRespone.java
@@ -4,7 +4,10 @@ package com.example.backend.dto.response;
 import java.time.LocalDateTime;
 
 public interface GioHangRespone {
+    public String getID();
     public String getMa();
+    public String getNhanVien();
+    public String getKhachHang();
     public String getNguoiTao();
     public String getNguoiSua();
     public LocalDateTime getNgayTao();
