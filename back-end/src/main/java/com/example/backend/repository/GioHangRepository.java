3c33bde98bec9a6700a61215afce32329f4b0ab4
anhntpph28990@fpt.edu.vn <anhntpph28990@fpt.edu.vn>
2024-02-19 16:48:45 +0700

    giỏ hàng

--- back-end/src/main/java/com/example/backend/repository/GioHangRepository.java
+++ back-end/src/main/java/com/example/backend/repository/GioHangRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface GioHangRepository extends JpaRepository<GioHang,String> {
     @Query(value = """
-        SELECT gh.id,gh.ma,gh.nhan_vien_id as nhanVien ,gh.khach_hang_id as khachHang,gh.nguoi_tao as nguoiTao,gh.nguoi_sua as nguoiSua,gh.ngay_tao as ngayTao,gh.ngay_sua as ngaySua,gh.trang_thai as trangThai from gio_hang gh join nguoi_dung on gh.khach_hang_id=nguoi_dung.id where gh.khach_hang_id=:idKH
+        SELECT gh.id,gh.ma,gh.nhan_vien_id as nhanVien ,gh.khach_hang_id as khachHang,gh.nguoi_tao as nguoiTao,gh.nguoi_sua as nguoiSua,gh.ngay_tao as ngayTao,gh.ngay_sua as ngaySua,gh.trang_thai as trangThai from gio_hang gh join nguoi_dung on gh.khach_hang_id=nguoi_dung.id where gh.khach_hang_id=:idKH and gh.trang_thai=0
 """,nativeQuery = true)
     GioHangRespone detailGioHang(String idKH);
     @Query(value = """
