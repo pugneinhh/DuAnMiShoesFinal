740ff25483f7206c712e0a6019d533970bf2f405
Paiigio <duongntph29011@fpt.edu.vn>
2024-04-02 20:02:39 +0700

    giao diện trả hàng

---
+++ reactapp/src/pages/censor/traHang-managenment/traHang.js
@@ -0,0 +1,101 @@
+import {
+  Button,
+  Divider,
+  Form,
+  Input,
+  Modal,
+  Select,
+  Slider,
+  Table,
+  Space,
+  Tag,
+  Badge,
+  Col,
+  Row,
+} from "antd";
+import { useEffect, useState } from "react";
+import { GiReturnArrow } from "react-icons/gi";
+const TraHang = () => {
+  const [form] = Form.useForm();
+
+  return (
+    <div className="container-fuild">
+      <div
+        className="  m-2 p-3 pt-2"
+        style={{
+          backgroundColor: "rgb(255, 255, 255)",
+          border: "1px solid #ddd", // Border color
+          boxShadow: "0 3px 8px rgba(0, 0, 0, 0.1)", // Box shadow
+          borderRadius: "8px",
+          maxWidth: "100%", // Đảm bảo div không vượt quá kích thước màn hình
+          height: "800px",
+        }}
+      >
+        <GiReturnArrow size={30} />{" "}
+        <span className=" fs-5">
+          <b>Trả hàng</b>
+        </span>
+        <div className="row d-flex justify-content-center mt-5">
+          <Form
+            form={form}
+            style={{ paddingLeft: 400 }}
+            // onFinish={handleSubmit}
+          >
+            <Row>
+              <Col span={11} style={{ marginTop: "32px" }}>
+                <Form.Item
+                  name="ma"
+                  label={
+                    <span style={{ fontWeight: "bold", fontSize: "16px" }}>
+                      Mã hóa đơn
+                    </span>
+                  }
+                  tooltip="Vui lòng nhập mã hóa đơn"
+                >
+                  <Input style={{ width: 400 }} />
+                </Form.Item>
+              </Col>
+              <Col className="d-flex align-items-center ms-2 mt-2">
+                <Button
+                  style={{
+                    // width: "110px",
+                    // height: "40px",
+                    // margin: "0 10px 10px 10px ",
+                    backgroundColor: "#3366CC",
+                    color: "white",
+                  }}
+                  // htmlType="reset"
+                >
+                  Tìm kiếm
+                </Button>
+              </Col>
+              <Col className="d-flex align-items-center ms-2 mt-2">
+                <Button
+                  style={{
+                    // width: "110px",
+                    // height: "40px",
+                    // margin: "0 10px 10px 10px ",
+                    backgroundColor: "#3366CC",
+                    color: "white",
+                  }}
+                  // htmlType="reset"
+                >
+                  QRCode
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+        </div>
+        <div className="text-center mt-4">
+          <img
+            src={
+              "https://img.freepik.com/premium-vector/worker-loading-truck_530733-3017.jpg?w=1380"
+            }
+            style={{ width: 1000, height: 500 }}
+          ></img>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default TraHang;
