3f555169eaee0480792e797c88c9682bf634e54c
anhpqph29035 <anhpqph29035@fpt.edu.vn>
2024-04-23 19:58:17 +0700

    hien thi anh admin+client

--- reactapp/src/pages/censor/sanPham-management/SuaAnhCTSP.js
+++ reactapp/src/pages/censor/sanPham-management/SuaAnhCTSP.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { PlusOutlined } from '@ant-design/icons';
 import { Modal, Upload } from 'antd';
+import { SanPhamAPI } from '../api/SanPham/sanPham.api';
 const getBase64 = (file) =>
   new Promise((resolve, reject) => {
     const reader = new FileReader();
@@ -8,18 +9,29 @@ const getBase64 = (file) =>
     reader.onload = () => resolve(reader.result);
     reader.onerror = (error) => reject(error);
   });
-const SuaAnhCTSP = ({hinhAnh}) => {
+const SuaAnhCTSP = ({ten,idSP}) => {
   const [previewOpen, setPreviewOpen] = useState(false);
   const [previewImage, setPreviewImage] = useState('');
-  const [previewTitle, setPreviewTitle] = useState('');
-  const [fileList, setFileList] = useState([
-    {
-      uid: '-1',
-      name: 'Nike Shoes.png',
-      status: 'done',
-      url: `${hinhAnh}`,
-    }
-  ]);
+  const [previewTitle, setPreviewTitle] = useState('');  
+  const [fileList, setFileList] = useState([]);
+  const loadAnhCTSP = async () => {
+    SanPhamAPI.getAnhCTSP(ten,idSP)
+    .then((res)=>{   
+      const newFileList = res.data.map((anh, index) => ({
+        uid: `${index}`,
+        name: `Image-${index}`,
+        status: 'done',
+        url: anh.url,
+      }));
+      setFileList(newFileList);
+    })
+  };
+
+  useEffect(() => {
+    loadAnhCTSP();
+  }, [ten,idSP]);
+
+
   const handleCancel = () => setPreviewOpen(false);
   const handlePreview = async (file) => {
     if (!file.url && !file.preview) {
